2049daf4f9e10c1f9d388fed569f57b44bb7a5c4
valdecir <desenvolvimento@axetecnologia.com.br>
2020-04-14 18:53:51 -0300

    algumas informações a mais da api do tempo

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,8 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 const messageThree = document.querySelector('#message-3')
 const messageFour = document.querySelector('#message-4')
+const messageFive = document.querySelector('#message-5')
+const messageSix = document.querySelector('#message-6')
 
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
@@ -12,6 +14,9 @@ weatherForm.addEventListener('submit',(e)=>{
     messageTwo.textContent= ''
     messageThree.textContent = ''
     messageFour.textContent = ''
+    messageFive.textContent = ''
+    messageSix.textContent = ''
+
     const location = search.value
     fetch('/weather/?adress='+ location).then((response)=>{
         response.json().then((data)=>{
@@ -19,8 +24,12 @@ weatherForm.addEventListener('submit',(e)=>{
                  
                 return messageOne.textContent=data.error
             }
+            
             messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
+            messageTwo.textContent = data.forecast.temperatura  
+            messageFive.textContent = data.forecast.umidade
+            messageSix.textContent = data.forecast.descricao
+
             messageFour.textContent = 'Latitude: '+ data.latitude 
             messageThree.textContent = 'Longitude: '+ data.longitude
             
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,10 +10,13 @@ forecast = (longitude, latitude, callback) => {
         else if (response.body.error){            
             callback('Unable to find location!',undefined)
         } else {
-            callback(undefined,'A temperatura atual é de '
+            callback(undefined,{temperatura: 'A temperatura atual é de '
                                 +response.body.current.temperature+
-                                ' Graus C e a sensação termica é '+
-                                response.body.current.feelslike + ' Graus C')
+                                ' graus C e a sensação termica é '+
+                                response.body.current.feelslike + ' graus C.',
+                                umidade: ' A umidade do ar está em '+
+                                response.body.current.humidity+ '%',
+                            descricao: 'O tempo está '+ response.body.current.weather_descriptions[0]+ '(  A api weatherstack free nao traduz. Vamos melhorar isso!)'  })
         }
                
     })   
